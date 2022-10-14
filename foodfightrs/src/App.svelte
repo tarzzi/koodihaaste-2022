769c183095cc51a4e0595a70fc2649eca3558ca6
Tarmo Urrio <5585433+tarzzi@users.noreply.github.com>
2022-10-14 22:22:45 +0300

    Fruit selection

--- foodfightrs/src/App.svelte
+++ foodfightrs/src/App.svelte
@@ -6,6 +6,43 @@
   <h1>FooD FighTRS</h1>
 
   <div class="grid">
+    <div class="card">
+      <select>
+        <option>Test1</option>
+        <option>Test2</option>
+        <option>Test3</option>
+        <option>Test4</option>
+      </select>
+      <h2>APPLE</h2>
+      <Fruit
+        color="blue"
+        name="apple"
+        hp="42"
+        atk="5"
+        def="6"
+        gre="0.5"
+        rng="201"
+      />
+    </div>
+    <div class="card">VS.</div>
+    <div class="card">
+      <select>
+        <option>Test1</option>
+        <option>Test2</option>
+        <option>Test3</option>
+        <option>Test4</option>
+      </select>
+      <h2>PEAR</h2>
+      <Fruit
+        color="red"
+        name="pear"
+        hp="64"
+        atk="2"
+        def="7"
+        gre="0.9"
+        rng="202"
+      />
+    </div>
   </div>
 </main>
 
