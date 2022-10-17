b6b42c70902183e4409007345cc5b3b8031bd56e
Tarmo Urrio <5585433+tarzzi@users.noreply.github.com>
2022-10-17 22:22:40 +0300

    Styling and branding, assign champ images

--- foodfightrs/src/lib/Title.svelte
+++ foodfightrs/src/lib/Title.svelte
@@ -1,11 +1,17 @@
 <script>
 </script>
-
-<h1>VFL</h1>
-<h3>Veggie Fighters League</h3>
+<div>
+<h1>FFL</h1>
+<h3>Food Fighters League</h3>
 <h2>Select your champions</h2>
-
+</div>
 <style>
+  div{
+    background-color: rgba(255, 255, 255, 0.726);
+    border-top-left-radius: 25%;
+    border-top-right-radius: 25%;
+    padding-bottom: 1em;
+  }
   h1 {
     margin-bottom: 0;
     padding-bottom: 0;
